7b0402911bdc09603e0c49e8e1a7786ad79d8ff9
shinebayar-g <shinebayar.gansukh@gmail.com>
2020-09-02 11:20:00 -0500

    ci: debug envs

--- src/app.py
+++ src/app.py
@@ -1,11 +1,10 @@
 from flask import Flask
 from flask import request
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    print("----")
-
     return f"Hello, {request.environ.get('HTTP_X_REAL_IP', request.remote_addr)}!"
 
 if __name__ == "__main__":
