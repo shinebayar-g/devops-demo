42af79f90cffbc11ea826cc23f6cd80bf603df79
shinebayar-g <shinebayar.gansukh@gmail.com>
2020-09-02 03:06:06 -0500

    feat: return visitor IP address

--- src/app.py
+++ src/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    print("----")
+
+    return f"Hello, {request.environ.get('HTTP_X_REAL_IP', request.remote_addr)}!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
