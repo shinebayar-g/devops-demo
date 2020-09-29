8976cc06aef94f9dba4beae831910fc60858afe7
shinebayar-g <shinebayar.gansukh@gmail.com>
2020-09-29 12:35:58 -0500

    flask: fix lint errors

--- python/my-app/app.py
+++ python/my-app/app.py
@@ -1,10 +1,12 @@
-from flask import Flask, request, json
+from flask import Flask, request
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return f"Hello, {request.environ.get('HTTP_X_REAL_IP', request.remote_addr)}!"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
